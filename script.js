7a74668c5c849796f1e3ae93fef540b91c1bb803
ericpkatz <ericpkatz@gmail.com>
2023-08-08 14:10:07 -0400

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+    //create a firstName and a lastName
+    const firstName = 'prof';
+    const lastName = 'katz';
+
+    //create a message with firstName and lastName
+    //template literal
+    const message = `Welcome ${firstName} ${lastName} !!!`;
+
+    //alert the message
+    //alert(message);
+
+    //create 3 numbers
+    const firstNumber = 49 / 7;
+
+    //create a message
+    const message2 = `The combination is ${firstNumber}-`;
+
+    //alert the message to the user
+    alert(message2);
